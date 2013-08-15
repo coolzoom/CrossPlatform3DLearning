ab0540a42fa1f9d6dd76ff4c657c705d2091957a
Dimitrios Kourkoulis <developer@dimitros.net>
2013-08-15 13:57:54 +0200

    Rotating cube in Linux

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,13 +1,143 @@
-#ifdef __cplusplus
-    #include <cstdlib>
-#else
-    #include <stdlib.h>
-#endif
-#ifdef __APPLE__
-#include <SDL/SDL.h>
-#else
+#include <cstdlib>
+
 #include <SDL.h>
-#endif
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+float angle = 0;
+
+bool light;
+bool lp;
+bool fp;
+
+GLfloat xrot;
+GLfloat yrot;
+GLfloat xspeed;
+GLfloat yspeed;
+GLfloat z=-0.5f;
+
+GLfloat LightAmbient[] = {0.5f, 0.5f, 0.5f, 1.0f};
+GLfloat LightDiffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
+GLfloat LightPosition[] = {-5.0f, 0.0f, 2.0f, 1.0f};
+
+GLuint filter;
+GLuint texture[3];
+
+void InitGL(int Width, int Height)	        // We call this right after our OpenGL window is created.
+{
+
+    //NeHeLoadBitmap("Untitled.bmp", texture[0]);
+    glEnable(GL_TEXTURE_2D);
+
+    glViewport(0, 0, Width, Height);
+    glShadeModel(GL_SMOOTH);			// Enables Smooth Color Shading
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);		// This Will Clear The Background Color To Black
+    glClearDepth(1.0f);				// Enables Clearing Of The Depth Buffer
+    glEnable(GL_DEPTH_TEST);			// Enables Depth Testing
+    glDepthFunc(GL_LEQUAL);				// The Type Of Depth Test To Do
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);			// Really Nice Perspective Calculations
+
+    glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmbient);				// Setup The Ambient Light
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDiffuse);				// Setup The Diffuse Light
+    glLightfv(GL_LIGHT1, GL_POSITION,LightPosition);			// Position The Light
+
+    glEnable(GL_LIGHT1);							// Enable Light One
+    glEnable(GL_LIGHTING);
+    glColor4f(1.0f,1.0f,1.0f,0.5f);			// Full Brightness, 50% Alpha ( NEW )
+    glBlendFunc(GL_SRC_ALPHA,GL_ONE);		// Blending Function For Translucency Based On Source Alpha Value ( NEW )
+    glEnable(GL_BLEND);		// Turn Blending On
+    glDisable(GL_DEPTH_TEST);
+
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();				// Reset The Projection Matrix
+
+    gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,100.0f);	// Calculate The Aspect Ratio Of The Window
+
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void DrawGLScene()
+{
+    if (angle > 360) angle = 0;
+    angle += 3;
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		// Clear The Screen And The Depth Buffer
+
+    glLoadIdentity();
+    glTranslatef(0.0f, 0.0f, -7.0f);
+
+    glRotatef(angle,1.0f,1.0f,0.0f);
+    glBegin(GL_QUADS);				// start drawing a polygon (4 sided)
+
+    // Front Face
+    glNormal3f( 0.0f, 0.0f, 1.0f);		// Normal Pointing Towards Viewer
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f,  1.0f);	// Point 1 (Front)
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f( 1.0f, -1.0f,  1.0f);	// Point 2 (Front)
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f( 1.0f,  1.0f,  1.0f);	// Point 3 (Front)
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-1.0f,  1.0f,  1.0f);	// Point 4 (Front)
+    // Back Face
+    glNormal3f( 0.0f, 0.0f,-1.0f);					// Normal Pointing Away From Viewer
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);	// Point 1 (Back)
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(-1.0f,  1.0f, -1.0f);	// Point 2 (Back)
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f( 1.0f,  1.0f, -1.0f);	// Point 3 (Back)
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f( 1.0f, -1.0f, -1.0f);	// Point 4 (Back)
+    // Top Face
+    glNormal3f( 0.0f, 1.0f, 0.0f);					// Normal Pointing Up
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-1.0f,  1.0f, -1.0f);	// Point 1 (Top)
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-1.0f,  1.0f,  1.0f);	// Point 2 (Top)
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f( 1.0f,  1.0f,  1.0f);	// Point 3 (Top)
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f( 1.0f,  1.0f, -1.0f);	// Point 4 (Top)
+    // Bottom Face
+    glNormal3f( 0.0f,-1.0f, 0.0f);					// Normal Pointing Down
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);	// Point 1 (Bottom)
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f( 1.0f, -1.0f, -1.0f);	// Point 2 (Bottom)
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f( 1.0f, -1.0f,  1.0f);	// Point 3 (Bottom)
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f,  1.0f);	// Point 4 (Bottom)
+    // Right face
+    glNormal3f( 1.0f, 0.0f, 0.0f);					// Normal Pointing Right
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f( 1.0f, -1.0f, -1.0f);	// Point 1 (Right)
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f( 1.0f,  1.0f, -1.0f);	// Point 2 (Right)
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f( 1.0f,  1.0f,  1.0f);	// Point 3 (Right)
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f( 1.0f, -1.0f,  1.0f);	// Point 4 (Right)
+    // Left Face
+    glNormal3f(-1.0f, 0.0f, 0.0f);					// Normal Pointing Left
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);	// Point 1 (Left)
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f,  1.0f);	// Point 2 (Left)
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(-1.0f,  1.0f,  1.0f);	// Point 3 (Left)
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-1.0f,  1.0f, -1.0f);	// Point 4 (Left)
+
+
+    glEnd();					// done with the polygon
+
+    // swap buffers to display, since we're double buffered.
+    SDL_GL_SwapBuffers();
+}
 
 int main ( int argc, char** argv )
 {
@@ -18,76 +148,57 @@ int main ( int argc, char** argv )
         return 1;
     }
 
-    // make sure SDL cleans up before exit
     atexit(SDL_Quit);
 
-    // create a new window
-    SDL_Surface* screen = SDL_SetVideoMode(640, 480, 16,
-                                           SDL_HWSURFACE|SDL_DOUBLEBUF);
+    SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+    SDL_WM_SetCaption("SDL Application", "SDL Test");
+
+    SDL_Surface *screen;
+    SDL_Surface *icon = SDL_LoadBMP("ONLINE32.BMP");
+    SDL_WM_SetIcon(icon, NULL);
+
+    screen = SDL_SetVideoMode( 1024, 768, 32, SDL_OPENGL); // *** SDL_HWSURFACE //DL_OPENGL |SDL_FULLSCREEN SDL_HWSURFACE SDL_DOUBLEBUF | );
+    InitGL(1024, 768);
+
     if ( !screen )
     {
-        printf("Unable to set 640x480 video: %s\n", SDL_GetError());
+        printf("Unable to set video: %s\n", SDL_GetError());
         return 1;
     }
-
-    // load an image
-    SDL_Surface* bmp = SDL_LoadBMP("cb.bmp");
-    if (!bmp)
-    {
-        printf("Unable to load bitmap: %s\n", SDL_GetError());
-        return 1;
-    }
-    
-    // centre the bitmap on screen
-    SDL_Rect dstrect;
-    dstrect.x = (screen->w - bmp->w) / 2;
-    dstrect.y = (screen->h - bmp->h) / 2;
 
     // program main loop
     bool done = false;
     while (!done)
     {
-        // message processing loop
+
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
-            // check for messages
             switch (event.type)
             {
-                // exit if the window is closed
+
             case SDL_QUIT:
                 done = true;
                 break;
 
-                // check for keypresses
             case SDL_KEYDOWN:
-                {
-                    // exit if ESCAPE is pressed
-                    if (event.key.keysym.sym == SDLK_ESCAPE)
-                        done = true;
-                    break;
-                }
-            } // end switch
-        } // end of message processing
+            {
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                    done = true;
+                break;
+            }
+            }
+        }
+        DrawGLScene();
+    }
 
-        // DRAWING STARTS HERE
-        
-        // clear screen
-        SDL_FillRect(screen, 0, SDL_MapRGB(screen->format, 0, 0, 0));
+    SDL_FreeSurface(icon);
+    SDL_FreeSurface(screen);
 
-        // draw bitmap
-        SDL_BlitSurface(bmp, 0, screen, &dstrect);
-
-        // DRAWING ENDS HERE
-
-        // finally, update the screen :)
-        SDL_Flip(screen);
-    } // end main loop
-
-    // free loaded bitmap
-    SDL_FreeSurface(bmp);
-
-    // all is well ;)
     printf("Exited cleanly\n");
     return 0;
 }
