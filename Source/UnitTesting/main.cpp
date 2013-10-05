28175a0e9e1b8a05807d16120d94941a58c57261
Dimitrios Kourkoulis <developer@dimitros.net>
2013-10-05 08:20:59 +0200

    Adding Boost Unit Test Framework (not done yet)

---
+++ Source/UnitTesting/main.cpp
@@ -0,0 +1,18 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+using namespace boost::unit_test;
+
+void my_test_function()
+{
+    int a = 5;
+}
+
+
+test_suite* init_unit_test_suite ( int argc, char* argv[] )
+{
+    test_suite* test = BOOST_TEST_SUITE( "Master test suite" );
+
+    test->add( BOOST_TEST_CASE( &my_test_function ) );
+
+    return test;
+}
