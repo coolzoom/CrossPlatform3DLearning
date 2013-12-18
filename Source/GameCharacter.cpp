238ebc29a0d856ffa5664d5805db6cb9821c8c55
Dimitrios Kourkoulis <developer@dimitros.be>
2013-12-18 23:56:20 +0100

    Splitting the lines of the OBJ file into tokens

--- Source/GameCharacter.cpp
+++ Source/GameCharacter.cpp
@@ -1,28 +1,32 @@
 #include "GameCharacter.h"
 #include <iostream>
 #include <fstream>
+#include <boost/foreach.hpp>
+#include <boost/tokenizer.hpp>
 
-GameCharacter::GameCharacter(void)
-{
+using namespace std;
+using namespace boost;
+
+GameCharacter::GameCharacter(void) {
 }
 
-
-GameCharacter::~GameCharacter(void)
-{
+GameCharacter::~GameCharacter(void) {
 }
 
-void GameCharacter::LoadFromFile( string fileLocation )
-{
+void GameCharacter::LoadFromFile(string fileLocation) {
 	ifstream file(fileLocation.c_str());
 	string line;
-	if (file.is_open())
-	{
-		while (getline(file, line))
-		{
-			cout << line << endl;
+	if (file.is_open()) {
+		cout << "Tokens:" << endl;
+		while (getline(file, line)) {
+			char_separator<char> sep(" ");
+			tokenizer<char_separator<char> > tokens(line, sep);
+			BOOST_FOREACH (const string& t, tokens){
+			cout << t << endl;
 		}
-		file.close();
 	}
-	else cout << "Could not open file." << endl;
-		
+	file.close();
+}
+else cout << "Could not open file." << endl;
+
 }
