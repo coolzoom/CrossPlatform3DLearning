b1cbc83fed61223a306ce5607609050cc6097619
Dimitrios Kourkoulis <developer@dimitros.be>
2014-01-19 10:00:13 +0100

    Added Configuration class

---
+++ Source/Configuration.cpp
@@ -0,0 +1,21 @@
+/*
+ * Configuration.cpp
+ *
+ *  Created on: Jan 19, 2014
+ *      Author: me
+ */
+
+#include "Configuration.h"
+
+namespace AvoidTheBug3D {
+
+Configuration::Configuration() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Configuration::~Configuration() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace AvoidTheBug3D */
---
+++ Source/Configuration.h
@@ -0,0 +1,21 @@
+/*
+ * Configuration.h
+ *
+ *  Created on: Jan 19, 2014
+ *      Author: me
+ */
+
+#ifndef CONFIGURATION_H_
+#define CONFIGURATION_H_
+
+namespace AvoidTheBug3D {
+
+class Configuration {
+public:
+	Configuration();
+	virtual ~Configuration();
+};
+
+} /* namespace AvoidTheBug3D */
+
+#endif /* CONFIGURATION_H_ */
