ef2bf61493b137a9b7b0516bd571bde3b36ef378
Dimitrios Kourkoulis <developer@dimitros.be>
2014-01-25 07:21:40 +0100

    Creating home directory finding function

--- Source/Configuration.cpp
+++ Source/Configuration.cpp
@@ -8,6 +8,11 @@
 #include "Configuration.h"
 
 #include <boost/filesystem.hpp>
+#include <algorithm>
+#include <boost/algorithm/string/predicate.hpp>
+#include "GameException.h"
+
+using namespace std;
 
 namespace AvoidTheBug3D {
 
@@ -21,9 +26,40 @@ Configuration::~Configuration() {
 }
 
 string Configuration::getHomeDirectory() {
-	boost::filesystem::path current;
-	current = boost::filesystem::current_path();
-	return string(current.c_str());
+	string current = string(boost::filesystem::current_path().c_str());
+    string currentLower = current;
+    transform(currentLower.begin(), currentLower.end(), currentLower.begin(), ::tolower);
+    string homeDirectory;
+    if (boost::algorithm::ends_with(currentLower, "unittesting")) // Linux unit testing
+    {
+
+    }
+    else if (boost::algorithm::ends_with(currentLower, "temp")) // Linux IDE execution
+    {
+
+    }
+    else if (boost::algorithm::ends_with(currentLower, "debug") ||
+    		boost::algorithm::ends_with(currentLower, "release")) // Windows
+    {
+    	if (currentLower.find("unittesting")) // Unit testing
+    	{
+
+    	}
+    	else // IDE execution
+    	{
+
+    	}
+    }
+    else if (boost::algorithm::ends_with(currentLower, "bin")) // production
+    {
+
+    }
+    else // if no conditions is met, the home directory cannot be determined
+    {
+    	throw GameException("Could not determine the home directory");
+
+    }
+	return homeDirectory;
 }
 
 
