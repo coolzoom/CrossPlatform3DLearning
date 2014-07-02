94477927fdb88da020c3164e8ef19d075d1bae0d
Dimitri Kourkoulis <developer@dimitros.be>
2014-07-02 07:33:32 +0200

    Added Image class + preparing to use libpng

---
+++ Source/Image.cpp
@@ -0,0 +1,18 @@
+/*
+ * Image.cpp
+ *
+ *  Created on: 2 Jul 2014
+ *      Author: me
+ */
+
+#include "Image.h"
+
+Image::Image() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Image::~Image() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Source/Image.h
@@ -0,0 +1,17 @@
+/*
+ * Image.h
+ *
+ *  Created on: 2 Jul 2014
+ *      Author: me
+ */
+
+#ifndef IMAGE_H_
+#define IMAGE_H_
+
+class Image {
+public:
+	Image();
+	virtual ~Image();
+};
+
+#endif /* IMAGE_H_ */
