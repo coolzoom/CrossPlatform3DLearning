c7c125d8cf3aa755c28d7993506562eae35c73d8
Dimitri Kourkoulis <developer@dimitros.be>
2014-08-06 05:55:54 +0200

    Linux correction compensating for Windows libpng correction

--- Source/Image.h
+++ Source/Image.h
@@ -15,11 +15,12 @@
 #include "GameLog.h"
 #include "Configuration.h"
 #include <png.h>
+#ifdef _WIN32
 #include <pngstruct.h> // needed by pnginfo.h
 #include <pnginfo.h> // Not needed directly but otherwise the error
 					 // "C4150: deletion of pointer to incomplete type"
 					 // will show up.
-
+#endif
 
 using namespace std;
 
