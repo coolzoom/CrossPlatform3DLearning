7805931bbc70e65849a61850cb4414b625220ad9
Dimitri Kourkoulis <developer@dimitros.be>
2014-08-12 06:21:20 +0200

    At least now I know that there is an invalid operation error when I upload the texture image

--- Source/RendererOpenGL33.cpp
+++ Source/RendererOpenGL33.cpp
@@ -137,17 +137,25 @@ namespace AvoidTheBug3D {
 							glGenTextures(1, &texture);
 							glBindTexture(GL_TEXTURE_2D, texture);
 
-							glTexStorage2D(GL_TEXTURE_2D, 0, GL_RED, 3 * textureObj->getWidth(),
-								3 * textureObj->getHeight());
-							glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 3 * textureObj->getWidth(),
-								3 * textureObj->getHeight(), GL_RED, GL_UNSIGNED_SHORT,
-								(GLvoid *)textureObj->getData());
+							/*glTexStorage2D(GL_TEXTURE_2D, 0, GL_RGB, textureObj->getWidth(),
+							textureObj->getHeight());
+							glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, textureObj->getWidth(),
+							textureObj->getHeight(), GL_RGB, GL_UNSIGNED_SHORT,
+							(GLvoid *)textureObj->getData());*/
+							//glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+							glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textureObj->getWidth(), textureObj->getHeight(), 0, 
+								GL_RGB, GL_UNSIGNED_SHORT, textureObj->getData());
+							GLenum errorCode = glGetError();
+							if (errorCode != GL_NO_ERROR)
+							{
+								throw GameException(string((char*)gluErrorString(errorCode)));
+							}
 							/*glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
 							glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);*/
 
 							/*glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 							glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-*/
+							*/
 							glBindTexture(GL_TEXTURE_2D, 0);
 							textures->insert(boost::unordered_map<string, GLuint>::value_type(it->get()->getName(), texture));
 						}
